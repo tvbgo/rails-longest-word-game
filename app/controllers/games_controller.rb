5fe5dfe2ca7db6bae4471a233c5cc0ab50679490
Tomas Gomes <tvb.gomes@gmail.com>
2020-11-17 11:31:52 -0300

    scss improvements

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,9 +5,24 @@ class GamesController < ApplicationController
 
   def score
     @answer = params[:answer]
-    url = "https://wagon-dictionary.herokuapp.com/#{@answer}"
-    @serialized = open(url).read
-    @parsed = JSON.parse(@serialized)
-    @parsed.symbolize_keys!
+    @url = "https://wagon-dictionary.herokuapp.com/#{@answer}"
+    @chars = @answer.chars
+    get_result(@url)
+  end
+
+  def check_letters(chars)
+    chars.each do |char|
+      @letters.include? "#{char}"
+    end
+  end
+
+  def get_result(url)
+    serialized = open(url).read
+    parsed = JSON.parse(serialized)
+    parsed.symbolize_keys!
+    @score = parsed[:length]
+    @word = parsed[:word]
+    @found = parsed[:found]
+    @welcome = parsed[:message]
   end
 end
